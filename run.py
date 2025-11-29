bd5dc727ed88a0427b25c1ba4671f851f406ff12
WordpressDesigne <sir.wanjigo@gmail.com>
2025-11-29 23:10:32 +0300

    fifth commit

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def make_shell_context():
 
 @app.cli.command()
 def test():
-    ""Run the unit tests."""
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
