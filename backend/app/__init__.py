b9a836f101edad4f112ba23870349c1f2e505bf5
WordpressDesigne <sir.wanjigo@gmail.com>
2026-01-05 16:02:57 +0300

    fix: Correct import path for Config in app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from config import Config
+from backend.config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
